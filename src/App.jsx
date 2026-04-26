ea434cafab5d2fd48bb0a45819638cabf4c66145
samkosal <67755908+samkosal@users.noreply.github.com>
2026-04-25 19:11:16 -0700

    init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+/**
+ * Challenge: Build the Header component in a separate file
+ * and render it here in the App component
+ */
+
+import Header from "./Header"
+
+export default function App() {
+  return (
+      <Header />
+  )
+}
---
+++ src/Header.jsx
@@ -0,0 +1,10 @@
+import chefClaudeLogo from './images/Chef_Icon.png'
+
+export default function Header() {
+    return (
+        <header>
+            <img src={chefClaudeLogo}/>
+            <h1>Chef Claude</h1>
+        </header>
+    )
+}
