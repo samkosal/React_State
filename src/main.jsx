ea079eba4d4c7a9b36b38d7c366f7dc59e3f05f8
samkosal <67755908+samkosal@users.noreply.github.com>
2026-05-01 23:40:32 -0700

    conditiontal rendering for ingredients list

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,22 @@
+import React from "react";
+
+
 export default function Main() {
-    const ingredients = ["Chicken", "Oregano", "Tomatoes"]
+    // const ingredients = ["Chicken", "Oregano", "Tomatoes"]
+    const [ingredients, setIngredients] = React.useState([]);
     
     const ingredientsListItems = ingredients.map(ingredient => (
         <li key={ingredient}>{ingredient}</li>
     ))
 
-    function handleSubmit(event) {
-        event.preventDefault()
-        const formData = new FormData(event.currentTarget)
+    function addIngredient(formData) {
         const newIngredient = formData.get("ingredient")
-        ingredients.push(newIngredient)
+        setIngredients(prevIngredients => [...prevIngredients, newIngredient])
     }
     
     return (
         <main>
-            <form onSubmit={handleSubmit} className="add-ingredient-form">
+            <form action={addIngredient} className="add-ingredient-form">
                 <input 
                     type="text"
                     placeholder="e.g. oregano"
@@ -23,9 +25,19 @@ export default function Main() {
                 />
                 <button>Add ingredient</button>
             </form>
-            <ul>
-                {ingredientsListItems}
-            </ul>
+            {ingredients.length > 0 && <section>
+                <h2>Ingredients on hand:</h2>
+                <ul className="ingredients-list" aria-live="polite">{ingredientsListItems}</ul>
+                {ingredients.length > 3 &&<div className="get-recipe-container">
+                    <div>
+                        <h3>Ready for a recipe?</h3>
+                        <p>Generate a recipe from your list of ingredients.</p>
+                    </div>
+                    <button>Get a recipe</button>
+                </div>}
+            </section>}
         </main>
     )
 }
+
+// 
